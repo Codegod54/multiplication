80376c2e4b3cac2472165deabc7d0ae00ed97101
Basant Chaudhary <bmagar932@gmail.com>
2023-12-05 17:19:24 +0545

    -

---
+++ multiplication/polls/multiplication.py
@@ -0,0 +1,11 @@
+# Program to Find the multiplication of a Number
+#num = int(input("Enter number:"))
+num = 5
+class mul:
+
+    for i in range(11):
+
+        mult = i * num
+
+        print("Multiplication of ", num,"*", i, "=", mult)
+
